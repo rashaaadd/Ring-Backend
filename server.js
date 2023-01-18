7053b5a537e904f65ab4b5e3567946cac0851bf5
rashaaadd <2017uee1217@mnit.ac.in>
2023-01-18 16:57:05 +0530

    Socket implemented

--- server.js
+++ server.js
@@ -10,23 +10,67 @@ const adminRoute = require('./routes/adminRoutes')
 const chatRoute = require('./routes/chatRoutes')
 const messageRoute = require('./routes/messageRoutes')
 
-const port =  process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 connectDB()
 
 const app = express()
 
-app.use(cors())
-app.use(express.json({ limit: '50mb'}))
-app.use(express.urlencoded({ limit: '50mb',extended: false })) 
+const { createServer } = require('http')
+const httpServer = createServer(app);
+const { Server } = require('socket.io');
 
-app.use('/',userRoute)
-app.use('/post',postRoute)
-app.use('/admin',adminRoute)
-app.use('/chat',chatRoute)
-app.use('/message',messageRoute)
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: ["http://localhost:3000","https://master.d3mn9a96s3gfxu.amplifyapp.com", "https://www.master.d3mn9a96s3gfxu.amplifyapp.com"]
+    },
+});
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+    // Add new User
+    socket.on("new-user-add", (newUserId) => {
+        //if user not added previously
+        if (!activeUsers.some((user) => user.userId === newUserId)) {
+            activeUsers.push({
+                userId: newUserId,
+                socketId: socket.id,
+            });
+        }
+        console.log("Connected users", activeUsers);
+        io.emit("get-users", activeUsers);
+    });
+
+    socket.on("disconnect", () => {
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+        console.log("User Disconnected", activeUsers);
+        io.emit("get-users", activeUsers);
+    });
+    //send message
+    socket.on("send-message", (data) => {
+        const { recieverId } = data;
+        const user = activeUsers.find((user) => user.userId === recieverId);
+        console.log("Sending from socket to :", recieverId);
+        console.log(user, "Data", data);
+        if (user) {
+            console.log(data, "hshahdjajklsdkj");
+            io.to(user?.socketId).emit("recieve-message", data);
+        }
+    });
+});
+
+app.use(cors())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: false }))
+
+app.use('/', userRoute)
+app.use('/post', postRoute)
+app.use('/admin', adminRoute)
+app.use('/chat', chatRoute)
+app.use('/message', messageRoute)
 
 
 app.use(errorHandler)
 
-app.listen(port,()=> console.log(`Server connected to port ${port}`))
+app.listen(port, () => console.log(`Server connected to port ${port}`))
